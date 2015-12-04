c79081612e8f863e450d5da306c1c3f1d667d278
rui725 <rui21rafael@gmail.com>
2015-12-04 02:13:57 -0500

    Answers for question 1

---
+++ question1.py
@@ -0,0 +1,9 @@
+first_names = ['Shan', 'Dao Ming', 'Hua Ze', 'Xi', 'Mei']
+last_names = ['Cai', 'Si', 'Lei', 'Men', 'Zuo']
+full_names = []
+for f in first_names:
+    for l in last_names:
+       full_names.append(f + ' '+ l)
+
+print full_names
+
