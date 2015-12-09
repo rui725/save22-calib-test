6ef264bdc4d8c1aab9e507f869d8f1f21245e52b
rui725 <rui21rafael@gmail.com>
2015-12-08 23:26:44 -0500

    TURTLE EXERCISE for Letter L

---
+++ turtle_exercise.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle.penup()
+turtle.backward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.pendown()
+turtle.backward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.exitonclick()
